81916af2b38759998eaf6969d3fe260a15a2dcf8
LadPaule <ssozipaule@gmail.com>
2021-08-27 09:51:18 +0300

    tes

--- keys.js
+++ keys.js
@@ -1,7 +1,7 @@
 // App key https://developer.google.com/custom-search/v1/using_rest
 
-export const API_KEY= process.env.API_KEY;
+export const API_KEY= "AIzaSyBcaYIwHGdwU4P7n6qNIdn3leWlBxgJm2Q";
 
 // context key - https://cse.google.com/cse/create/new
-export const CONTEXT_KEY=process.env.CONTEXT_KEY;
+export const CONTEXT_KEY="d86e0eb6b6bca1094";
 
